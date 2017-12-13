38c18556458a2eca29ac3209416de5e2983f7d2e
Stavros Papantonakis <lardianos@yahoo.gr>
2017-12-13 14:00:27 +0200

    KY-028 Sensor Test Code

--- firecarexample/firecarexample.ino
+++ firecarexample/firecarexample.ino
@@ -1,1 +1,37 @@
+// KY-028
+int AnalogInput = A1;
+int DigitalInput = 3;
 
+void setup ()
+{
+  Serial.begin (9600);
+  pinMode (AnalogInput, INPUT);
+  pinMode (DigitalInput, INPUT);
+}  
+
+void loop ()
+{
+  float Analog;
+  int Digital;
+  Analog = analogRead(DigitalInput);
+  Digital= digitalRead(DigitalInput);
+  Serial.print(Analog);
+  Serial.print(" -- ");
+  Serial.println(Digital);
+  /* Current values will be read and converted to voltage
+  Analog = analogRead (Analog_Eingang) * (5.0 / 1023.0); 
+  Digital = digitalRead (Digital_Eingang);
+  
+  Serial.print ("Analog voltage value:"); Serial.print (Analog, 4);  Serial.print ("V, ");Serial.print (Digital);
+  Serial.print ("Extreme value:");
+  
+  if(Digital==1)
+  {
+      Serial.println (" reached");
+  }
+  else
+  {
+      Serial.println (" not yet reached");
+  }*/
+  delay (200);
+}
